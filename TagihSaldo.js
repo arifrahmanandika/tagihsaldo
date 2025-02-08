ab52a96c8941c27360787ec206a75d86c3465d92
arifrahmanandika <136570114+arifrahmanandika@users.noreply.github.com>
2025-02-08 13:43:54 +0700

    Update TagihSaldo.js

--- TagihSaldo.js
+++ TagihSaldo.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { Client, LocalAuth } = require("whatsapp-web.js"); // Tambahkan LocalAuth di sini
+const puppeteer = require('puppeteer-core');
 const qrcode = require("qrcode-terminal");
 const cors = require("cors");
 
@@ -12,6 +13,9 @@ app.use(bodyParser.json());
 // Membuat client WhatsApp dengan LocalAuth untuk menyimpan sesi
 const client = new Client({
   authStrategy: new LocalAuth(),
+  puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
 });
 
 // Menampilkan QR code di terminal untuk autentikasi pertama kali
